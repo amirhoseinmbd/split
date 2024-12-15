22c11b88ca4349af8a7e075943cde16b2795106d
amirmbd <amirmbd2014@gmail.com>
2024-12-16 02:04:23 +0330

    v1.4

--- import sqlite3.py
+++ import sqlite3.py
@@ -9,40 +9,40 @@ money REAL   NOT NULL
 )
     '''
 
+
     def __init__(self):
         self.con = sqlite3.connect('split.db')
         self.cur = self.con.cursor()
         self.cur.execute(self.table)
+        self.debtor_payer_list()
         # self.enter_tarakonesh()
         # self.show_data()
-        self.debtor_payer_list()
         
-        
-
     
     def enter_tarakonesh(self):
-        while (insert := input('Do you want to insert transaction : ') != 'no'):
-            print(id := len(self.cur.execute('SELECT * FROM split_table').fetchall())+1) 
-            self.id = id
-            self.payer = input('Enter name of the payer : ')
-            self.cost = float(input('Enter the cost : ')) 
-            self.contribute = int(input('Enter the number of person evolve :  '))
-            self.portion = self.cost / self.contribute
-            self.enter_check_payer()
+       while (input('Do you want to insert transaction : ') != 'no'):
+            try:
+                self.id = len(self.cur.execute('SELECT * FROM split_table').fetchall())+1 
+                self.payer = input('Enter name of the payer : ')
+                self.cost = float(input('Enter the cost : ')) 
+                self.contribute = int(input('Enter the number of person evolve :  '))
+                self.portion = self.cost / self.contribute
+                self.enter_check_payer()
+                    
+
+                for i in range (self.contribute-1):
+                    self.id += 1
+                    self.person = input('Enter the name : ')
+                    self.enter_check_debtor()
+
             
-
-            for i in range (self.contribute-1):
-                self.id += 1
-                self.person = input('Enter the name : ')
-                self.enter_check_debtor()
-
-            if (qestion := input("continue yes/no ? ")) != 'no':...
-            else:
-                break
+            except ValueError : 
+                print('Error! Enter right values') 
+                continue
 
         
     def show_data(self):
-        if (show := input('Do you want to see data base : ') != 'no'):
+        if (input('Do you want to see data base : ') != 'no'):
             self._show = self.cur.execute(' SELECT * FROM split_table').fetchall()
             for row in self._show :
                 print(row)
@@ -53,9 +53,9 @@ money REAL   NOT NULL
         
         if exist : 
             updated_money = exist[1] + (self.portion * (self.contribute-1))
-            self.cur.execute('UPDATE split_table SET money = ? WHERE id = ? ',(updated_money,exist[0]))
+            self.cur.execute('UPDATE split_table SET money = ? WHERE id = ? ',(round(updated_money,3),exist[0]))
 
-        else : self.cur.execute('INSERT INTO split_table VALUES (?,?,?)',(self.id, self.payer, self.portion * (self.contribute-1)))
+        else : self.cur.execute('INSERT INTO split_table VALUES (?,?,?)',(self.id, self.payer, round(self.portion * (self.contribute-1),3)))
 
 
     def enter_check_debtor(self):
@@ -63,9 +63,10 @@ money REAL   NOT NULL
         
         if exist : 
             updated_money = exist[1] + (-(self.portion))
-            self.cur.execute('UPDATE split_table SET money = ? WHERE id = ? ',(updated_money,exist[0]))
+            self.cur.execute('UPDATE split_table SET money = ? WHERE id = ? ',(round(updated_money,3),exist[0]))
 
-        else : self.cur.execute('INSERT INTO split_table VALUES (?,?,?)',(self.id, self.person, -(self.portion)))
+        else : self.cur.execute('INSERT INTO split_table VALUES (?,?,?)',(self.id, self.person , round(-(self.portion),3)))
+
 
     def debtor_payer_list(self):
         self.payer_list = self.cur.execute('SELECT name,money FROM split_table WHERE money > 0').fetchall()
@@ -73,40 +74,68 @@ money REAL   NOT NULL
 
         self.debtor_list = self.cur.execute('SELECT name,money FROM split_table WHERE money < 0').fetchall()
         self.debtor_list.sort(key = lambda x : x[1])
-        print(self.payer_list)
-        print(self.debtor_list)
-
-    def balance(self): 
-        # while True:
-        for m in range (10):
-            for i in range(len(self.payer_list)) : 
-                money_payer = self.payer_list[i][1]
-                if money_payer == 0 :
-                    print(f'{self.money_payer[i][0]} is balanced. ')
-                    self.payer_list.pop(i)
-
-            for j in range(len(self.debtor_list)) : 
-                money_debtor = self.debtor_list[j][1]
-                if money_debtor == 0 :
-                    print(f'{self.money_debtor[j][0]} is balanced. ')
-                    self.debtor_list.pop(j)
-
-                      
-            # for person_payer in self.payer_list: 
-            #     self.money_payer = person_payer[1] 
-
-            #     for person_debtor in self.debtor_list: 
-            #         self.money_debtor = person_debtor[1]
-
-            #         if self.money_payer == -(self.money_debtor) :
-            #             person_payer[1] = 0
-            #             person_debtor[1] = 0
-            #             print(f"{person_debtor[0]} to {person_payer[0]} : {self.money_payer}")
+        print(f'payer list is : {self.payer_list}\n ___________________________________________')
+        print(f'debtor list is : {self.debtor_list}\n ___________________________________________')
 
 
+    def balance(self):
+        while True:
+            if len(self.debtor_list) == 0 : print('_____Now All are Balance_____') ; break
 
-        
+            else :
+                for i in range(len(self.payer_list)) : 
+                    money_payer = self.payer_list[i][1]
+                    if money_payer == 0 :
+                        self.payer_list.pop(i)
+
+                for j in range(len(self.debtor_list)) : 
+                    money_debtor = self.debtor_list[j][1]
+                    if money_debtor == 0 :
+                        self.debtor_list.pop(j)
+
+                        
+                for person_payer in self.payer_list: 
+                    self.money_payer = person_payer[1] 
+
+                    for person_debtor in self.debtor_list: 
+                        self.money_debtor = person_debtor[1]
+
+                        if self.money_payer == -(self.money_debtor) :
+                            self.payer_list.pop(self.payer_list.index((person_payer[0],person_payer[1])))
+                            self.debtor_list.pop(self.debtor_list.index((person_debtor[0],person_debtor[1])))
+                            print(f"{person_debtor[0]} to {person_payer[0]} : {self.money_payer}")
+
+                if len(self.debtor_list) == 0 : print('_____Now All are Balance_____') ;break
+
+                else : 
+                    if -(self.debtor_list[0][1]) > self.payer_list[0][1]:
+                        print(f"{self.debtor_list[0][0]} to {self.payer_list[0][0]} : {self.payer_list[0][1]}")
+                        remain = self.debtor_list[0][1] + self.payer_list[0][1]
+                        self.debtor_list.append((self.debtor_list[0][0],remain))
+                        self.debtor_list.pop(0)
+                        self.payer_list.pop(0)
+
+
+                    elif self.payer_list[0][1] > -(self.debtor_list[0][1]): 
+                        print(f"{self.debtor_list[0][0]} to {self.payer_list[0][0]} : {-(self.debtor_list[0][1])}")
+                        remain = self.payer_list[0][1] - self.debtor_list[0][1]
+                        self.payer_list.append((self.payer_list[0][0],remain))
+                        self.debtor_list.pop(0)
+                        self.payer_list.pop(0)
                     
+                    self.payer_list.sort(key = lambda x : x[1], reverse=True)
+                    self.debtor_list.sort(key = lambda x : x[1])
+                    
+
+
+
+
+
+            
+
+
+
+
 
     def pardakht(self):
         who_pay = input('Enter the payer : ')
@@ -115,14 +144,9 @@ money REAL   NOT NULL
 
         who_pay_money = self.cur.execute('SELECT money FROM split_table WHERE name = ? ',(who_pay,)).fetchone()
         who_recieve_money = self.cur.execute('SELECT money FROM split_table WHERE name = ? ',(who_recieve,)).fetchone()
-        print(who_pay_money)
-        print(who_recieve_money)
-
+        
         self.cur.execute('UPDATE split_table SET money = ? WHERE name = ? ',((who_pay_money[0]+amount),who_pay))
         self.cur.execute('UPDATE split_table SET money = ? WHERE name = ? ',((who_recieve_money[0]-amount),who_recieve))
-
-
-
 
 
     def end(self):
@@ -131,10 +155,15 @@ money REAL   NOT NULL
 
 
 
-amirSplit = Split()
 
+
+
+
+
+amirSplit = Split()
 # amirSplit.enter_tarakonesh()
+
 amirSplit.balance()
 # amirSplit.pardakht()
-amirSplit.show_data()
+# amirSplit.show_data()
 amirSplit.end()
